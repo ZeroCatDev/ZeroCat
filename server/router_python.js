9c363fbc7de261cb33b95342e446c6359d352290
SUNWUYUAN <1847261658@qq.com>
2023-11-19 16:26:51 +0800

    1

--- server/router_python.js
+++ server/router_python.js
@@ -197,7 +197,10 @@ router.post('/play/favo', function (req, res) {
 router.get('/edit', function (req, res) {
 	res.render('ejs/python/python_edit.ejs');
 })
-
+//python兼容编辑器
+router.get('/old', function (req, res) {
+	res.render('ejs/python/python_edit_old.ejs');
+})
 // 从数据库获取作品
 router.post('/getWork', function (req, res) {
     var projectid = 0;
