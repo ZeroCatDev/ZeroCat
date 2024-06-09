2657db65f7cf714b4ee85af3d800cad6fb619893
SunWuyuan <1847261658@qq.com>
2024-06-09 09:16:44 +0800

    1

--- server/lib/database.js
+++ server/lib/database.js
@@ -6,14 +6,14 @@ var pool = mysql.createPool({
      user: process.env.mysqluser,
      password: process.env.mysqlpassword,
      database: process.env.mysqldatabase,
-     debug: true
+     //debug: true
     });
 
 
 //防止注入：'SELECT * FROM ow_Users WHERE ?', WHERE
 exports.qww = function query_with_w(SQL, W, callback) {
     pool.getConnection(function (err, connection) {
-        console.log(err)
+        //console.log(err)
         if (err) return callback(err,'');
 
         // Use the connection
@@ -28,7 +28,7 @@ exports.qww = function query_with_w(SQL, W, callback) {
 //正常操作
 exports.query = function query(SQL, callback) {
     pool.getConnection(function (err, connection) {
-        console.log(err)
+        //console.log(err)
         if (err) return callback(err,'');
         
         // Use the connection
