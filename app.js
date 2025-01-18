baa48b6b3cf6f59f25e1f1cf44ac27bad197630c
SunWuyuan <1847261658@qq.com>
2025-01-18 21:22:15 +0800

    1

--- app.js
+++ app.js
@@ -160,7 +160,7 @@ app.get("/check", function (req, res, next) {
 });
 
 app.get("/scratchtool", function (req, res, next) {
-  res.status(200).sendFile(process.cwd() + "/scratchtool.js");
+  res.status(200).sendFile(process.cwd() + "/scratchtool.ejs");
 });
 process.on("uncaughtException", function (err) {
   logger.error(err);
